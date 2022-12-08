d1634434f31520a2e02fd5d789b018655a886dc7
Fabian Senftleben <Fabian.Senftleben@brockhaus-ag.de>
2022-12-08 12:45:39 +0100

    Added more tests and implementation of fizzbuzz

--- TestProjectFreitag/TestProjectFreitag/FizzBuzz.cs
+++ TestProjectFreitag/TestProjectFreitag/FizzBuzz.cs
@@ -9,6 +9,22 @@ public class FizzBuzz
 
     public string Run(int input)
     {
-        throw new NotImplementedException();
+        string fizzBuzzString = "";
+        if (input % 3 == 0)
+        {
+            fizzBuzzString += "Fizz";
+        }
+
+        if (input % 5 == 0)
+        {
+            fizzBuzzString += "Buzz";
+        }
+
+        if (fizzBuzzString.Equals(""))
+        {
+            fizzBuzzString += input.ToString();
+        }
+
+        return fizzBuzzString;
     }
 }
--- TestProjectFreitag/TestProjectFreitag/UnitTest1.cs
+++ TestProjectFreitag/TestProjectFreitag/UnitTest1.cs
@@ -3,6 +3,8 @@ namespace TestProjectFreitag;
 public class UnitTest1
 {
 
+    
+    
     [Fact]
     public void ConstructorTest_EmptyConstructor_NoException()
     {
@@ -10,14 +12,69 @@ public class UnitTest1
     }
 
     [Fact]
-    public void RunTest_Input1_Expects1()
+    public void RunTest_Input3_ExpectsFizz()
     {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        int input = 1;
-        string expected = "1";
+        int input = 3;
+        string expected = "Fizz";
 
         string actual = fizzBuzz.Run(input);
         
-        Assert.Equal(actual, expected);
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void RunTest_Input5_ExpectsBuzz()
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 5;
+        string expected = "Buzz";
+
+        string actual = fizzBuzz.Run(input);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void RunTest_Input15_ExpectsFizzBuzz()
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int input = 15;
+        string expected = "FizzBuzz";
+
+        string actual = fizzBuzz.Run(input);
+        
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [MemberData(nameof(InputStuff))]
+    public void TheoryTest(int input)
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        string expected = input.ToString();
+
+        string actual = fizzBuzz.Run(input);
+        
+        Assert.Equal(expected, actual);
+    }
+
+
+    public static IEnumerable<object[]> InputStuff()
+    {
+        yield return new object[] {1};
+        yield return new object[] {2};
+        yield return new object[] {4};
+        yield return new object[] {7};
+        yield return new object[] {8};
+        yield return new object[] {11};
+        yield return new object[] {13};
+        yield return new object[] {14};
+        yield return new object[] {16};
+        yield return new object[] {17};
+        yield return new object[] {19};
+        yield return new object[] {22};
+        yield return new object[] {23};
+        yield return new object[] {26};
     }
 }
